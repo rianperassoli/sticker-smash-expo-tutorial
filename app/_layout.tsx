d86b14714483bf22e615032edbdf81884ca95af9
Rian Perassoli <rian.perassoli@gmail.com>
2025-01-16 15:02:52 -0300

    adding status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
-import { Stack } from 'expo-router';
+import { Fragment } from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <Fragment>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+
+      <StatusBar style="light" />
+    </Fragment>
   );
 }
